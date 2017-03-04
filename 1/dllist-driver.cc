58fa48c2321e5bdcba965b91e210de76ce8f3765
mingyunyuansu <mingyunyuansu@163.com>
2017-03-04 15:46:24 +0800

    driver

--- 1/dllist-driver.cc
+++ 1/dllist-driver.cc
@@ -1,5 +1,10 @@
-void generateN(int N) {
+void generateN(int N, DLList * list) {
     for (int i = 0; i < N; ++i) {
         int num = rand();
+        list.SortedInsert(&num, num);
     }
+}
+
+void removeN(int N, DLList * list) {
+    
 }
